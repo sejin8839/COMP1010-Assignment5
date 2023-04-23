329c28d5eb652e81b75a86bc811690b4244b50a7
sejin8839 <sejinon01@gmail.com>
2023-04-23 22:30:36 +0900

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -1,4 +1,14 @@
 # Assignment written by: ___Sejin Yoon___
 
 
+sentence = input("Enter a sentence: ")
 
+words = sentence.split()  # split the sentence into words
+num_words = len(words)   # count the number of words
+total_length = sum(len(word) for word in words)  # sum the length of each word
+
+average_length = total_length / num_words  # calculate the average length
+
+print(words)
+print(len(words))
+print("The average word length in the sentence is:", round(average_length, 2))
